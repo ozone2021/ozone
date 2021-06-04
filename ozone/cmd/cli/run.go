f865bb790a2b6e9ac877cd0848460d4c6a9cced3
Jamie Holland <jholland@dnsfilter.com>
2021-06-03 23:43:45 -0400

    Return true

--- ozone/cmd/cli/run.go
+++ ozone/cmd/cli/run.go
@@ -80,7 +80,7 @@ func runIndividual(b *ozoneConfig.Runnable, context string, config *ozoneConfig.
 	buildScope["NAME"] = b.Name
 	buildScope = ozoneConfig.RenderNoMerge(buildScope, topLevelScope)
 
-	if cacheUpdate(buildScope) == false {
+	if checkCacheShouldRun(buildScope) == false {
 		log.Printf("Info: build %s is cached. \n", b.Name)
 		return nil
 	}
