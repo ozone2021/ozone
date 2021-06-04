f20093fc319ee1e6fb22604514387aafb08358ff
Jamie Holland <jholland@dnsfilter.com>
2021-06-04 17:12:10 -0400

    Halt index range error fixed.

--- ozone/cmd/cli/halt.go
+++ ozone/cmd/cli/halt.go
@@ -13,7 +13,10 @@ var haltCmd = &cobra.Command{
 	Use:   "h",
 	Long:  `Halts all services in current directory`,
 	Run: func(cmd *cobra.Command, args []string) {
-		service := args[0]
+		service := ""
+		if len(args) == 1 {
+			service = args[0]
+		}
 		process_manager_client.Halt(service)
 	},
 }
