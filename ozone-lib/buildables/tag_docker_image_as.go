4a2f163b8cb64392b4ff52976ff6a8b19fb85079
Jamie Holland <jamie.holland@tripstax.com>
2023-05-12 12:11:50 +0100

    Tag docker image.

--- ozone-lib/buildables/tag_docker_image_as.go
+++ ozone-lib/buildables/tag_docker_image_as.go
@@ -6,6 +6,7 @@ import (
 	process_manager "github.com/ozone2021/ozone/ozone-daemon-lib/process-manager"
 	. "github.com/ozone2021/ozone/ozone-lib/config/config_variable"
 	"github.com/ozone2021/ozone/ozone-lib/utils"
+	"log"
 	"os"
 	"os/exec"
 	"strings"
@@ -31,17 +32,31 @@ func TagDockerImageAs(varsMap *VariableMap) error {
 	if strings.Contains(targetTag.String(), "Merged branch is not release branch.") {
 		return errors.New("Merged branch is not release branch.")
 	}
-	cmdString := fmt.Sprintf("docker pull %s && docker tag %s %s", sourceTag, sourceTag, targetTag)
-	cmdFields, argFields := process_manager.CommandFromFields(cmdString)
-	cmd := exec.Command(cmdFields[0], argFields...)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stdout
-	err := cmd.Run()
+	log.Printf("Pulling docker image %s \n", sourceTag)
+	pullCmdString := fmt.Sprintf("docker pull %s", sourceTag)
+	cmdFields, argFields := process_manager.CommandFromFields(pullCmdString)
+	pullCmd := exec.Command(cmdFields[0], argFields...)
+	pullCmd.Stdout = os.Stdout
+	pullCmd.Stderr = os.Stdout
+	err := pullCmd.Run()
 	if err != nil {
-		fmt.Printf("%s\n", cmdString)
+		fmt.Printf("%s\n", pullCmdString)
 		return err
 	}
-	cmd.Wait()
+	pullCmd.Wait()
+
+	log.Printf("Tagging docker image %s as %s \n", sourceTag, targetTag)
+	tagCmdString := fmt.Sprintf("docker tag %s %s", sourceTag, targetTag)
+	tagCmdFields, argFields := process_manager.CommandFromFields(tagCmdString)
+	tagCmd := exec.Command(tagCmdFields[0], argFields...)
+	tagCmd.Stdout = os.Stdout
+	tagCmd.Stderr = os.Stdout
+	err = tagCmd.Run()
+	if err != nil {
+		fmt.Printf("%s\n", tagCmdString)
+		return err
+	}
+	tagCmd.Wait()
 
 	return nil
 }
